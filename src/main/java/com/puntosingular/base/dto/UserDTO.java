86015d37eaa63382f686cf0c38d7cfcb26fe4ffc
Brian Dominguez Balam <brianreach117@hotmail.com>
2020-02-25 10:31:55 -0500

    Subiendo cambios a Git Ultimo

--- src/main/java/com/puntosingular/base/dto/UserDTO.java
+++ src/main/java/com/puntosingular/base/dto/UserDTO.java
@@ -1,93 +1,59 @@
-package com.puntosingular.base.models;
+package com.puntosingular.base.dto;
 
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.*;
+import com.puntosingular.base.models.Role;
 
-@Entity
-@Table(name = "users")
-public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class UserDTO {
 
-	@Column(length = 45)
+    private Long id;
 	private String name;
-
-	@Column(length = 45)
 	private String lastname;
-
-	@Column(length = 255)
-	private String password;
-
-	@Column(nullable = false)
+	//private String password;
 	private Boolean status;
-
-	@Column(name = "create_date")
-	@Temporal(TemporalType.TIMESTAMP)
 	private Date createDate;
-
-	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_role"), uniqueConstraints = {
-			@UniqueConstraint(columnNames = { "id_user", "id_role" }) })
 	private List<Role> roles;
-
+	
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getLastname() {
 		return lastname;
 	}
-
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
+	
+	
 	public Boolean getStatus() {
 		return status;
 	}
-
 	public void setStatus(Boolean status) {
 		this.status = status;
 	}
-
 	public Date getCreateDate() {
 		return createDate;
 	}
-
 	public void setCreateDate(Date createDate) {
 		this.createDate = createDate;
 	}
-
 	public List<Role> getRoles() {
 		return roles;
 	}
-
 	public void setRoles(List<Role> roles) {
 		this.roles = roles;
 	}
-
+	
+	
 }
